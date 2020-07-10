916368e394fc950e86260963d0018f409de792b7
jcenny <jcenny26@gmail.com>
2020-07-09 20:22:09 -0700

    Complete length, uppercase, lowercase, and number validation

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -4,12 +4,36 @@ import PasswordForm from './PasswordForm.jsx';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      page: 'password',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePages = this.handlePages.bind(this);
+  }
+
+  handleSubmit(password) {
+    this.setState({
+      password,
+      page: 'login'
+    })
+  }
+
+  handlePages() {
+    const { page } = this.state;
+    if (page === 'password') {
+      return <PasswordForm handleSubmit={this.handleSubmit}/>
+    } else {
+      return <h1>Validate your email to continue...</h1>
+    }
   }
 
   render() {
     return (
       <div>
-        <PasswordForm />
+        {this.handlePages()}
       </div>
     )
   }
--- client/Components/PasswordForm.jsx
+++ client/Components/PasswordForm.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const List = styled.li`
+font-size: 10px;
+color: ${({color}) => `${color};`};
+`;
 
 class PasswordForm extends React.Component {
   constructor(props) {
@@ -6,9 +12,17 @@ class PasswordForm extends React.Component {
     this.state = {
       type: 'password',
       password: '',
+      length: 'black',
+      lowercase: 'black',
+      uppercase: 'black',
+      number: 'black',
+      email: 'black',
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleShow = this.handleShow.bind(this);
+    this.checkValidation = this.checkValidation.bind(this);
+    this.includesNumber = this.includesNumber.bind(this);
   }
 
   handleChange(event) {
@@ -17,8 +31,71 @@ class PasswordForm extends React.Component {
     this.setState({
       [name]: value
     }, () => {
-      console.log(`onchange: ${this.state.password}`)
+      this.checkValidation();
     })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { password } = this.state;
+    this.props.handleSubmit(password);
+  }
+
+  checkValidation() {
+    const { password } = this.state;
+    // check for password length
+    if (password.length >= 8) {
+      this.setState({
+        length: 'green',
+      })
+    } else {
+      this.setState({
+        length: 'black',
+      })
+    }
+    // check for lowercase and uppercase
+    if (password.toUppercase !== password && password.toLowerCase() !== password) {
+      this.setState({
+        lowercase: 'green',
+        uppercase: 'green'
+      })
+    } else if (password.toUpperCase() !== password && password.toLowerCase() === password) {
+      this.setState({
+        lowercase: 'green',
+        uppercase: 'black',
+      })
+    } else if (password.toUpperCase() === password && password.toLowerCase() !== password) {
+      this.setState({
+        uppercase: 'green',
+        lowercase: 'black',
+      })
+    } else {
+      this.setState({
+        uppercase: 'black',
+        lowercase: 'black',
+      })
+    }
+    // check for number
+    if (this.includesNumber(password)) {
+      this.setState({
+        number: 'green'
+      })
+    } else {
+      this.setState({
+        number: 'black'
+      })
+    }
+  }
+
+  includesNumber(password) {
+    let char;
+    for (let i = 0; i < password.length; i++) {
+      char = password[i];
+      if (!isNaN(char) && char !== ' ') {
+        return true;
+      }
+    }
+    return false;
   }
 
   handleShow() {
@@ -35,7 +112,7 @@ class PasswordForm extends React.Component {
   }
 
   render() {
-    const { password, type } = this.state;
+    const { password, type, length, lowercase, uppercase, number, email } = this.state;
     return (
       <div>
         <form>
@@ -43,14 +120,15 @@ class PasswordForm extends React.Component {
           <input type={type} name='password' value={password} onChange={this.handleChange} />
           <input type='checkbox' onClick={this.handleShow} />
           <label>Show</label>
+          <button onClick={this.props.handleSubmit}>Submit</button>
         </form>
         <div>Should contain</div>
         <ul>
-          <li>8-72 Characters</li>
-          <li>1 Lowercase Character</li>
-          <li>1 Uppercase Character</li>
-          <li>Number</li>
-          <li>Should Not Match Your Email Address</li>
+          <List color={length}>8-72 Characters</List>
+          <List color={lowercase}>1 Lowercase Character</List>
+          <List color={uppercase}>1 Uppercase Character</List>
+          <List color={number}>Number</List>
+          <List color={email}>Should Not Match Your Email Address</List>
         </ul>
       </div>
     )
