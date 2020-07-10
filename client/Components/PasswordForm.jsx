9f7f831673f8a0a248ff0673e8cc0c2215e4d7ad
jcenny <jcenny26@gmail.com>
2020-07-10 10:20:23 -0700

    Complete password confirmation

--- client/Components/PasswordForm.jsx
+++ client/Components/PasswordForm.jsx
@@ -20,10 +20,10 @@ const NavForm = styled.div`
 display: flex;
 align-items: center;
 justify-content: center;
-padding-top: 50px;
+padding-top: 60px;
 `
 const Input = styled.input`
-margin: 5px;
+margin-right: 10px;
 width: 400px;
 height: 25px;
 font-size: 14px;
@@ -34,8 +34,21 @@ const Label = styled.div`
 weight: 10px;
 font-size: 14px;
 color: grey;
-margin-left: 7px;
+margin-bottom: 5px;
+margin-left: 3px;
 font-weight: 400;
+`
+
+const Button = styled.button`
+margin-left: 10px;
+background-color: white;
+border: 1px solid grey;
+border-radius: 10px;
+color: black;
+padding: 5px 10px;
+text-align: center;
+text-decoration: none;
+font-size: 14px;
 `
 
 class PasswordForm extends React.Component {
@@ -51,6 +64,7 @@ class PasswordForm extends React.Component {
       number: false,
       email: false,
       validated: false,
+      matched: false,
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -59,6 +73,7 @@ class PasswordForm extends React.Component {
     this.checkEmail = this.checkEmail.bind(this);
     this.checkLetterCasing = this.checkLetterCasing.bind(this);
     this.includesNumber = this.includesNumber.bind(this);
+    this.isAllValidated = this.isAllValidated.bind(this);
     this.checkPasswordMatch = this.checkPasswordMatch.bind(this);
   }
 
@@ -68,7 +83,13 @@ class PasswordForm extends React.Component {
     this.setState({
       [name]: value
     }, () => {
-      this.checkValidation()})
+      if (name === 'password') {
+        this.checkValidation();
+        this.checkPasswordMatch();
+      } else {
+        this.checkPasswordMatch();
+      }
+    })
   }
 
   handleSubmit(event) {
@@ -105,6 +126,9 @@ class PasswordForm extends React.Component {
 
     // check for email 
     this.checkEmail();
+
+    // check for all validations
+    this.isAllValidated();
   }
 
   checkLetterCasing() {
@@ -158,15 +182,28 @@ class PasswordForm extends React.Component {
     return false;
   }
 
-  checkPasswordMatch() {
-    const { password, confirm } = this.state;
-    if (password === confirm) {
+  isAllValidated() {
+    const { length, lowercase, uppercase, number, email } = this.state;
+    if (length && lowercase && uppercase && number && email) {
       this.setState({
         validated: true,
       })
     } else {
       this.setState({
         validated: false,
+      })
+    }
+  }
+
+  checkPasswordMatch() {
+    const { password, confirm } = this.state;
+    if (password === confirm && confirm !== '' && password !== '') {
+      this.setState({
+        matched: true,
+      })
+    } else {
+      this.setState({
+        matched: false,
       })
     }
   }
@@ -185,14 +222,14 @@ class PasswordForm extends React.Component {
   }
 
   render() {
-    const { password, confirm, type, length, lowercase, uppercase, number, email, validated } = this.state;
+    const { password, confirm, type, length, lowercase, uppercase, number, email, validated, matched } = this.state;
     return (
       <NavForm>
         <form>
           {password ? <Label>Password</Label> : <Label></Label>}
           <Input type={type} name='password' placeholder='Password' value={password} onChange={this.handleChange} />
           <input type='checkbox' onClick={this.handleShow} />
-          <label>Show</label>
+          <label style={{color: 'grey', fontSize: '16px'}}>Show</label>
           <List className='fa-ul'> Must contain the following: 
             <ListItem color={!length ? 'black' : 'green'}>
               {!length ? '8-72 Characters' : <div>8-72 Characters <i className="fa-il fa fa-check-circle" aria-hidden="true"></i></div>}
@@ -212,8 +249,8 @@ class PasswordForm extends React.Component {
           </List>
           {confirm ? <Label>Confirm</Label> : ''}
           <Input type={type} placeholder='Confirm' name='confirm' value={confirm} onChange={this.handleChange} />
-          {validated ? <i className="fa fa-check-circle" aria-hidden="true"></i> : ''}
-          <button onClick={this.handleSubmit}>Submit</button>
+          <i className="fa fa-check-circle" aria-hidden="true" style={!matched ? {color: 'grey'} : {color: 'green'}}></i>
+          <Button onClick={this.handleSubmit}>Submit</Button>
         </form>
       </NavForm>
     )
